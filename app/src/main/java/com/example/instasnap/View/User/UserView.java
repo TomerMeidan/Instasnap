133ce8ecfcefa7166e9c5b276e698e16db518b56
Tomer Meidan <tomer.m@circ.zone>
2023-07-31 16:00:53 +0300

    Patched Broadcastq

--- app/src/main/java/com/example/instasnap/View/User/UserView.java
+++ app/src/main/java/com/example/instasnap/View/User/UserView.java
@@ -62,7 +62,7 @@ public class UserView extends AppCompatActivity {
     private boolean isMyServiceRunning(Class<?> serviceClass) {
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
+            if ("com.example.instasnap".equals(service.service.getPackageName())) {
                 return true;
             }
         }
